794f7f82f0956302d593ce660fad8928b685bb75
jahidhasan136 <jh3495689@gmail.com>
2025-02-06 18:12:04 +0600

    login page complete

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
+import LoginPage from "@/components/authPages/LoginPage";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="container mt-20">
+      <LoginPage />
     </div>
   );
 };
---
+++ src/components/authPages/LoginPage.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import SharedButton from "../shared/SharedButton";
+import Link from "next/link";
+import { LuEye, LuEyeOff } from "react-icons/lu";
+
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const LoginPage = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <div className="mx-auto max-w-[555px]">
+      <div className="text-center grid gap-4 mb-6">
+        <h1 className="font-bold text-[40px] leading-[48px] text-black">
+          Log in
+        </h1>
+        <p className="text-base font-medium capitalize leading-5 text-carbon">
+          Join Our 100% Free Creatieve Network
+        </p>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)} className="grid gap-10">
+        <div className="grid gap-4">
+          <div className="grid gap-3">
+            <label
+              htmlFor="email"
+              className="text-carbon text-base font-medium leading-[25px]"
+            >
+              Email*
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              className="px-4 py-[13px] bg-whiteSmoke rounded-lg text-gray text-sm h-12"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="grid gap-3">
+            <label
+              htmlFor="password"
+              className="text-carbon text-base font-medium leading-[25px]"
+            >
+              Password*
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="Enter your password"
+                className="w-full px-4 py-[13px] bg-whiteSmoke rounded-lg text-gray text-sm h-12"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: 6,
+                })}
+              />
+              <button
+                className="absolute right-5 top-1/2 h-4 w-4 -translate-y-1/2 transform"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <LuEye className="h-4 w-4" />
+                ) : (
+                  <LuEyeOff className="h-4 w-4" />
+                )}
+                <span className="sr-only">
+                  {showPassword ? "Hide password" : "Show password"}
+                </span>
+              </button>
+            </div>
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
+          </div>
+        </div>
+        <SharedButton type="submit" classes="w-full" text="sign up" />
+        <p className="font-medium leading-5 text-base text-center">
+          Have an account?{" "}
+          <Link href="/sign-up" className="text-primary font-bold underline">
+            sign up
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
