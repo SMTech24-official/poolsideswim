a98b5c8cebe02bf18efed2de7b6e35d4f2fd4fe2
jahidhasan136 <jh3495689@gmail.com>
2025-02-09 16:44:17 +0600

    banner responsive

--- src/components/shared/SharedBanner.tsx
+++ src/components/shared/SharedBanner.tsx
@@ -18,11 +18,11 @@ const SharedBanner = ({
         height={100}
         src={bannerImg}
         alt="Banner"
-        className="w-full"
+        className="w-full h-[450px] object-cover"
       />
       <div className="container">
         <div className="absolute top-0 bottom-0 grid place-content-center gap-6 text-white">
-          <h1 className="text-[64px] font-bold leading-[76px] capitalize">
+          <h1 className="text-5xl xl:text-[64px] font-bold xl:leading-[76px] capitalize">
             {title}
           </h1>
           <p className="text-base font-bold leading-5 flex items-center gap-3">
