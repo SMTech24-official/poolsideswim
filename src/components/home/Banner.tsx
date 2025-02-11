4b588a44dc97bd53e54066fcd37e7435ccb4419b
jahidhasan136 <jh3495689@gmail.com>
2025-02-11 09:52:10 +0600

    banner title fixed in client requirements

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -13,16 +13,18 @@ const Banner = () => {
         alt="Banner Image"
         className="w-full h-screen object-cover"
       />
-      <div className="container text-white absolute top-0 bottom-0 grid place-content-center">
-        <h1 className="text-5xl lg:text-[64px] font-bold xl:leading-[76px] mb-4">
-          Swimming made easy, fun, and convenient right in your own pool or
-          ours.
-        </h1>
-        <p className="text-base leading-[25px] mb-12">
-          Professional Swimming Lessons designed for all ages and skill levels.
-          Swimming Lessons made convenient.
-        </p>
-        <SharedButton text="Book Your Lessons" />
+      <div className="container">
+        <div className="text-white absolute top-0 bottom-0 grid place-content-center">
+          <h1 className="text-5xl lg:text-[64px] font-bold xl:leading-[76px] mb-4">
+            Swimming made easy, fun, <br /> and convenient right in your <br />{" "}
+            own pool or ours.
+          </h1>
+          <p className="text-base leading-[25px] mb-12">
+            Professional Swimming Lessons designed for all ages and skill
+            levels. Swimming Lessons made convenient.
+          </p>
+          <SharedButton text="Book Your Lessons" />
+        </div>
       </div>
     </div>
   );
