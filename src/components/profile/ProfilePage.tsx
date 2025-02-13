c397f3fcb6199e7c706841126658c564019fb811
jahidhasan136 <jh3495689@gmail.com>
2025-02-13 14:58:19 +0600

    profile page design

--- src/components/profile/ProfilePage.tsx
+++ src/components/profile/ProfilePage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import { IoIosLogOut } from "react-icons/io";
 import profileImg from "@/assets/gallery-1.svg";
 import Image from "next/image";
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { useRouter } from "next/navigation";
 import { removeUser } from "@/redux/slice/userSlice";
+import { LiaUserEditSolid } from "react-icons/lia";
 
 const ProfilePage = () => {
   const user = useSelector((state: RootState) => state?.user);
@@ -16,22 +17,45 @@ const ProfilePage = () => {
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const [image, setImage] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
   const handleLogout = () => {
     dispatch(removeUser());
     router.push("/login");
   };
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+    }
+  };
+  //   console.log(image);
   return (
     <div className="grid gap-6">
       <div className="flex items-center justify-between">
-        <div className="flex gap-4 items-center">
+        <div className="grid md:flex gap-4 items-center">
           {/* image */}
-          <div>
+          <div className="relative w-[84px] h-[84px]">
             <Image
               width={1000}
               height={1000}
-              src={profileImg}
+              src={image || profileImg}
               alt="Profile image"
               className="w-[84px] h-[84px] rounded-full object-cover"
+            />
+            <input
+              type="file"
+              ref={fileInputRef}
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageUpload}
+            />
+            <LiaUserEditSolid
+              className="absolute bottom-0 right-0 text-lg cursor-pointer"
+              onClick={() => fileInputRef.current?.click()}
             />
           </div>
           <div>
@@ -53,8 +77,8 @@ const ProfilePage = () => {
         </div>
       </div>
       <div className="grid gap-6">
-        <div className="flex justify-between gap-12">
-          <div className="grid gap-3 w-full">
+        <div className="md:flex justify-between gap-12">
+          <div className="grid gap-3 w-full mb-3 md:mb-0">
             <h2 className="text-base font-medium leading-[25px] text-black opacity-80">
               First Name *
             </h2>
@@ -71,8 +95,8 @@ const ProfilePage = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-between gap-12">
-          <div className="grid gap-3 w-full">
+        <div className="md:flex justify-between gap-12">
+          <div className="grid gap-3 w-full mb-3 md:mb-0">
             <h2 className="text-base font-medium leading-[25px] text-black opacity-80">
               Phone number *
             </h2>
