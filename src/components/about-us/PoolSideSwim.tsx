c1204f948fa98a0a935ac106e596b99c7422d4f3
jahidhasan136 <jh3495689@gmail.com>
2025-02-16 15:27:16 +0600

    poolSideSwim code updated with client requirements

--- src/components/about-us/PoolSideSwim.tsx
+++ src/components/about-us/PoolSideSwim.tsx
@@ -5,36 +5,13 @@ import aboutImage from "@/assets/about/aboutImage.png";
 const PoolSideSwim = () => {
   return (
     <div className="container">
-      <div className="grid xl:flex flex-row-reverse items-center gap-6 md:gap-12">
-        <Image
-          width={1000}
-          height={1000}
-          src={aboutImage}
-          alt="About Image"
-          className="md:w-[666px] md:h-[657px] rounded-lg"
-        />
-        <div className="grid gap-6 md:gap-12">
-          <div className="grid gap-4">
-            <h2 className="text-[40px] font-bold leading-[48px]">
-              Explore the poolside swimming
-            </h2>
-            <p className="text-base leading-[25px] text-gray">
-              At Poolside Swimming, we offer a range of lessons designed to help
-              swimmers of all ages and abilities grow and succeed. Whether your
-              child is just starting out or looking to refine their technique,
-              our expert instructors provide personalized, step-by-step
-              guidance. Safety, skill development, and building confidence are
-              at the heart of every class. Our state-of-the-art facilities
-              provide a comfortable, supportive environment for learning. From
-              group lessons to private coaching, we offer flexible options to
-              meet your family’s needs. We focus on making each lesson engaging
-              and fun, ensuring your child enjoys every moment in the water.
-              Explore our programs today and take the first step toward a
-              lifetime of swimming confidence and enjoyment.
-            </p>
-          </div>
-        </div>
-      </div>
+      <Image
+        width={1000}
+        height={1000}
+        src={aboutImage}
+        alt="About Image"
+        className="w-full md:h-[657px] rounded-lg object-cover "
+      />
     </div>
   );
 };
